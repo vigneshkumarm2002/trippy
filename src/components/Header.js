2f1b36ea5441ac056f45c3b716014b09ee5afdf1
vigneshkumarm2002 <mvigneshkumarbvn@gmail.com>
2023-09-12 14:29:49 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Logo from "./../assets/Trippy.png"
-import Avatar from "./../assets/avatar.jpg"
+import Avatar from "./../assets/profile-picture.jpg"
+import aeroSvg from "./../assets/aero.svg"
 
 const Header = styled.header`
   display: flex;
@@ -35,6 +36,33 @@ const ProfileImage = styled.img`
   height: 100%;
   border-radius: 50px;
 `;
+const AeroSvg =styled.img`
+width:300px;
+position:absolute;
+top:35%;
+left:0;
+@media only screen and (max-width: 1000px) {
+  width:300px;
+  top:40%;
+}
+@media only screen and (max-width: 800px) {
+  width:300px;
+  top:44%;
+}
+@media only screen and (max-width: 700px) {
+  width:250px;
+  top:38%;
+}
+@media only screen and (max-width: 520px) {
+  width:200px;
+  top:32%;
+  left:-50px;
+}
+@media only screen and (max-width: 335px) {
+  top:35%;
+  left:-80px;
+}
+`
 
 
 const YourComponent = () => {
@@ -44,6 +72,7 @@ const YourComponent = () => {
       <ProfileContainer onClick={() => alert("Under Development")}>
         <ProfileImage src={Avatar} alt="profile" />
       </ProfileContainer>
+      <AeroSvg src={aeroSvg} alt='illustration' />
     </Header>
   );
 };
